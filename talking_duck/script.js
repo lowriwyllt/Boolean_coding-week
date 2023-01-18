5c055925d2a88b440ae119e992b79a5e7387f8f0
lowriwyllt <lowriwyllt@live.co.uk>
2023-01-18 09:51:15 +0000

    Added files for the next sessions and edited the talking_duck to be a swan, and to change the gif to say 'I <3 code' if code is inputted anywhere in the textarea

--- talking_duck/script.js
+++ talking_duck/script.js
@@ -14,19 +14,24 @@
 const textArea = document.querySelector('textarea')
 const playButton = document.querySelector('button')
 const pitchBar = document.querySelector('input')
-const duckFigure = document.querySelector('figure')
+const swanFigure = document.querySelector('figure')
 
 
 // Control what happens when button is clicked
 // When clicked, check if there is text in the input field
 playButton.addEventListener('click', function () {
+    console.log(textArea.value.includes("code"))
     const textLength = textArea.value.trim().length
     if (textLength > 0) {
+        if(textArea.value.includes("code")==true){
+            speak_code()
+        } else {
         speak()
+        }
     }
 })
 
-// Function to make the duck talk!
+// Function to make the Swan talk!
 function speak() {
     // Retrieve text and audio values
     const text = textArea.value
@@ -48,22 +53,63 @@ function speak() {
     // utterance.voice = voice;
 
 
-    // Make the duck talk
+    // Make the Swan talk
     speechSynthesis.speak(utterance)
 
-    // When the duck begins to speak...
+    // When the Swan begins to speak...
     utterance.addEventListener('start', function () {
         textArea.disabled = true
         pitchBar.disabled = true
         playButton.disabled = true
-        duckFigure.classList.add('talking')
+        swanFigure.classList.add('talking')
     })
 
-    // When the duck has finished...
+    // When the swan has finished...
     utterance.addEventListener('end', function () {
         textArea.disabled = false
         pitchBar.disabled = false
         playButton.disabled = false
-        duckFigure.classList.remove('talking')
+        swanFigure.classList.remove('talking')
+    })
+}
+
+function speak_code() {
+    // Retrieve text and audio values
+    const text = textArea.value
+    const pitch = pitchBar.value
+
+    // Initialiase a new utterance
+    const utterance = new SpeechSynthesisUtterance(text)
+
+    // utterance.volume = 1 // 0 - 1
+    // utterance.rate = 0.5; // 0.1 - 10
+
+    // set the pitch level
+    utterance.pitch = pitch // 0 - 2
+
+    // // BONUS: set the voice
+    // const voice = voices.find(item => item.name.includes("CHOOSE ONE!"))
+    // console.log(voices, voice)
+    //
+    // utterance.voice = voice;
+
+
+    // Make the Swan talk
+    speechSynthesis.speak(utterance)
+
+    // When the Swan begins to speak...
+    utterance.addEventListener('start', function () {
+        textArea.disabled = true
+        pitchBar.disabled = true
+        playButton.disabled = true
+        swanFigure.classList.add('talking_code')
+    })
+
+    // When the Swan has finished...
+    utterance.addEventListener('end', function () {
+        textArea.disabled = false
+        pitchBar.disabled = false
+        playButton.disabled = false
+        swanFigure.classList.remove('talking_code')
     })
 }
